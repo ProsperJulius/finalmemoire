860454499acd2952694e9ed254d49d4917b313c9
prosper <prmavhunga@gmail.com>
2017-05-17 14:15:08 +0100

    food and data changes

--- app/src/main/java/com/example/prosper/safeairlines/Tabrepas.java
+++ app/src/main/java/com/example/prosper/safeairlines/Tabrepas.java
@@ -1,6 +1,11 @@
 package com.example.prosper.safeairlines;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 /**
  * Created by prosper on 30/4/2017.
@@ -8,5 +13,16 @@ import android.support.v4.app.Fragment;
 
 public class Tabrepas   extends Fragment {
 
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+       View view =inflater.inflate(R.layout.repas,container,false);
 
+
+
+
+
+
+        return view;
+    }
 }
