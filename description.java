96ed9495fac21413ad975e53b56d6e246d2a6955
Mavunga Prosper Julius <prmavhunga@gmail.com>
2019-12-15 16:12:12 +0100

    added printing method to the description class

--- description.java
+++ description.java
@@ -1,4 +1,8 @@
 public class description{
   public description(){
+    ///code to be added here
+  }
+  public void printSyst(){
+  System.out.println("Hey mr developer how are you doing");
   }
 }
